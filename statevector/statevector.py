6ad84a1194e5af45b597787eba39b133305634d5
crystal-tensor <cx95zz@126.com>
2024-07-03 17:59:46 +0800

    Update statevector.py

--- statevector/statevector.py
+++ statevector/statevector.py
@@ -1,27 +1,34 @@
-from setuptools import setup, find_packages
+import numpy as np
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.circuit.instruction import Instruction
+from qiskit.exceptions import QiskitError
+from qiskit.quantum_info.states.quantum_state import QuantumState
+from qiskit.quantum_info.operators.mixins.tolerances import TolerancesMixin
+from qiskit.quantum_info.operators.operator import Operator
+from qiskit.quantum_info.operators.symplectic import Pauli, SparsePauliOp
+from qiskit.quantum_info.operators.op_shape import OpShape
+from qiskit.quantum_info.operators.predicates import matrix_equal
 
-setup(
-    name='statevector',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'scikit-learn',
-    ],
-    entry_points={
-        'console_scripts': [],
-    },
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A vector database optimized for cosine similarity using quantum algorithms',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/crystal-tensor/StateVector',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
+from qiskit._accelerate.pauli_expval import (
+    expval_pauli_no_x,
+    expval_pauli_with_x,
 )
+
+class Statevector(QuantumState, TolerancesMixin):
+    """Statevector class"""
+    # (Your existing code here...)
+
+class StateVectorDB:
+    """Simple class to store and retrieve state vectors."""
+    
+    def __init__(self):
+        self.vectors = {}
+
+    def add(self, vector, ids):
+        for i, id in enumerate(ids):
+            self.vectors[id] = vector[i]
+
+    def get(self, id):
+        return self.vectors.get(id, None)
+
+# Your existing Statevector class implementation goes here...
